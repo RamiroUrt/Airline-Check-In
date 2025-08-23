3665208518f963cd07eb04294e4ec38b8c31a4af
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-08-23 13:41:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
