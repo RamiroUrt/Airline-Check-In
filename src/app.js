f11784ff5671b17f645465a54c77f77b78a4d319
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-08-23 13:58:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import testRouter from "./routes/test/test.js";
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ 
@@ -12,13 +14,6 @@ app.get("/", (req, res) => {
     timestamp: new Date().toISOString()
   });
 });
-
-app.use(cors());
-app.use(express.json());
-app.use("/flights", flightsRouter);
-
-app.use("/test", testRouter);
-
 //endpoint para Raleway Health Check
 app.get("/health", (req, res) => {
   res.status(200).json({ 
@@ -26,6 +21,11 @@ app.get("/health", (req, res) => {
     timestamp: new Date().toISOString() 
   });
 });
+
+app.use("/flights", flightsRouter);
+
+app.use("/test", testRouter);
+
 
 
 export default app;
