dc0abd6910a42f670888818fda5874baa31c9198
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-08-23 17:47:37 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,4 +7,9 @@ const pool = mysql.createPool(
   process.env.DATABASE_URL || process.env.MYSQL_URL || process.env.MYSQLDATABASE_URL
 );
 
+
+if (!connectionString) {
+  throw new Error("❌ DATABASE_URL no está definida.");
+}
+
 export default pool;
